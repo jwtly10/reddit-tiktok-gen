e672fa504f8e1ab3a77861964d5e9f0de8993183
Josh Watley <watleyjosh@gmail.com>
2024-04-15 11:51:28 +0100

    feat: wip video generation from content

---
+++ app/service/generate.py
@@ -0,0 +1,115 @@
+import os
+from app.utils.elevenlabs import ElevenLabs
+from app.utils.gentle_aligner import GentleAligner
+from app.utils.openai import determine_gender_from_text
+from app.utils.ffmpeg import (
+    get_audio_duration,
+    buffer_audio,
+    loop_video_to_audio,
+    concatenate_audios,
+    delay_srt,
+    embed_srt_and_audio,
+    overlay_image_on_video,
+    get_video_dimensions,
+    resize_image,
+    FFMpegProcessingError,
+)
+from app.utils.background_video import get_random_chunk_from_video
+
+import app.config
+
+from app.utils.logger import log
+
+
+def generate_video_from_content(
+    id: str, title: str, content: str, base_background_video: str, output_dir: str
+):
+    """
+    Generate a video from the given content.
+
+    Args:
+        id (str): The ID of the video.
+        title (str): The title of the video.
+        content (str): The content of the video.
+        base_background_video (str): The path to the base background video.
+        output_dir (str): The directory where the generated media will be saved.
+
+    Raises:
+        FFMpegProcessingError: If an error occurs during video processing using ffmpeg.
+        Exception: If an unexpected error occurs.
+
+    Returns:
+        None
+    """
+    try:
+        log.info("Generating video from content")
+        log.debug(f"ID: {id}")
+        log.debug(f"Title: {title}")
+        log.debug(f"Content: {content}")
+        log.debug(f"Base Background Video: {base_background_video}")
+        log.debug(f"Output Directory: {output_dir}")
+
+        os.makedirs(output_dir, exist_ok=True)
+
+        gender = determine_gender_from_text(content)
+
+        elevenlabs = ElevenLabs()
+        pre_content_audio = os.path.join(output_dir, "pre_content.mp3")
+        elevenlabs.generate_mp3(content, gender, pre_content_audio)
+
+        content_audio = os.path.join(output_dir, "content.mp3")
+        buffer_audio(pre_content_audio, "END", 1, content_audio)
+
+        gentle_aligner = GentleAligner()
+        aligned_text = gentle_aligner.generate_aligned(content, content_audio)
+
+        srt_file = os.path.join(output_dir, "content.srt")
+        gentle_aligner.generate_srt(aligned_text, srt_file)
+
+        pre_title_audio = os.path.join(output_dir, "pre_title.mp3")
+        elevenlabs.generate_mp3(title, gender, pre_title_audio)
+        pre_title_audio_duration = get_audio_duration(pre_title_audio)
+
+        title_audio = os.path.join(output_dir, "title.mp3")
+        buffer_audio(pre_title_audio, "END", 1, title_audio)
+        title_audio_duration = get_audio_duration(title_audio)
+
+        # TODO: Add logic to generate title image
+        title_image = os.path.join("base_background_media", "reddit_title_template.png")
+
+        video_audio = os.path.join(output_dir, "video.mp3")
+        concatenate_audios(title_audio, content_audio, video_audio)
+        total_video_audio_length = get_audio_duration(video_audio)
+
+        background_video = os.path.join(output_dir, "background.mp4")
+        get_random_chunk_from_video(base_background_video, 60, background_video)
+
+        looped_background_video = os.path.join(output_dir, "looped_background.mp4")
+        loop_video_to_audio(
+            total_video_audio_length, background_video, looped_background_video
+        )
+
+        video_width = get_video_dimensions(looped_background_video)[0]
+        resized_title_image = os.path.join(output_dir, "resized_title_image.png")
+        resize_image(title_image, video_width, resized_title_image)
+
+        overlayed_video = os.path.join(output_dir, "overlayed.mp4")
+        overlay_image_on_video(
+            looped_background_video,
+            resized_title_image,
+            pre_title_audio_duration,
+            overlayed_video,
+        )
+
+        delayed_srt_file = os.path.join(output_dir, "delayed_content.srt")
+        delay_srt(srt_file, title_audio_duration, delayed_srt_file)
+
+        final_video = os.path.join(output_dir, "final.mp4")
+        embed_srt_and_audio(overlayed_video, video_audio, delayed_srt_file, final_video)
+
+        log.info(f"Final video generated at {final_video}")
+
+    except FFMpegProcessingError as e:
+        log.error(f"{e}: {e.stderr}")
+    except Exception as e:
+        log.error(f"An unexpected error occurred while generating video: {e}")
---
+++ tests/integration/service/test_generate.py
@@ -0,0 +1,54 @@
+import unittest
+import os
+from unittest.mock import patch, MagicMock
+import app.service.generate as generate
+
+
+from app.service.generate import generate_video_from_content
+
+
+class TestVideoGeneration(unittest.TestCase):
+    @patch("app.service.generate.ElevenLabs")
+    @patch("app.service.generate.determine_gender_from_text")
+    def test_generate_video_from_content(self, mock_determine_gender, mock_elevenlabs):
+        mock_determine_gender.return_value = "m"
+
+        mock_elevenlabs_instance = mock_elevenlabs.return_value
+        mock_elevenlabs_instance.generate_mp3.return_value = None
+
+        test_post = """
+        I'll keep it brief because it's so timely. Am I the asshole because it's Father's Day and all I wanted to do was spend time alone in the backyard, smoking a cigar, and listening to music/podcasts by myself? I recognize without kids, I am not a subject of this holiday, but my wife and kids are giving the pass and I'm taking it. Yet part of me still wonders...
+        """
+        test_title = """
+        AITA Short and sweet
+        """
+        test_id = "tempid"
+
+        test_base_vid_path = os.path.join(
+            "base_background_media", "minecraft_background_video_1.mp4"
+        )
+
+        output_dir = os.path.join(
+            "tests",
+            "fixtures",
+            "integration",
+            "service",
+            "generate",
+            # , id
+        )
+
+        generate.generate_video_from_content(
+            test_id, test_title, test_post, test_base_vid_path, output_dir
+        )
+
+        expected_final_video_path = os.path.join(output_dir, "final.mp4")
+
+        self.assertTrue(os.path.exists(expected_final_video_path))
+        self.assertTrue(os.path.getsize(expected_final_video_path) > 0)
+
+    def tearDown(self):
+        """Clean up anything thats not needed after test"""
+
+
+if __name__ == "__main__":
+    unittest.main()
