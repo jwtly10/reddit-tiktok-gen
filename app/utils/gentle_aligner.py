4d82b83ec3585f527464e3cfdc04ed8a7847db2e
Josh Watley <watleyjosh@gmail.com>
2024-04-16 21:57:30 +0100

    fix: improving srt generation

--- app/utils/gentle_aligner.py
+++ app/utils/gentle_aligner.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 from typing import cast
+import re
 
 from app.utils.logger import log
 
@@ -29,6 +30,8 @@ class GentleAligner:
 
         log.info("Aligning content")
         url = os.path.join(self.baseUrl, "transcriptions?async=false")
+
+        transcript = self.clean_up_transcript(transcript)
 
         response = None
         with open(audio_file_path, "rb") as audio_file:
@@ -76,22 +79,104 @@ class GentleAligner:
             f"Type of aligned_object (should be json/dict): {type(aligned_object)}"
         )
 
-        for item in aligned_object["words"]:
-            if item["case"] != "success":
-                log.debug(f"Error aligning the item: {item}")
+        counter = 1
+        srt_content = ""
+        group_size = 3
+        temp_words = []
+        temp_start = None
+        temp_end = None
+
+
+        len_of_aligned_object = len(aligned_object["words"])
+
+        # TODO: Clean up this code
+        for i in range(0, len_of_aligned_object, 1):
+            if aligned_object["words"][i]["case"] != "success":
+                    log.debug(f"Error aligning the item: {aligned_object['words'][i]}")
+                    continue
+
+            # Set the start time of the group
+            if not temp_start:
+                temp_start = aligned_object["words"][i]["start"]
+
+            # Update the end time to the last word's end time in the group
+            temp_end = aligned_object["words"][i]["end"]
+            temp_words.append(aligned_object["words"][i]["word"])
+
+            # If it's the last word, write the group
+            if i == len_of_aligned_object - 1:
+                start_time = int(temp_start * 1000)
+                end_time = int(temp_end * 1000)
+
+                start_srt = f"{start_time // 3600000:02}:{(start_time % 3600000) // 60000:02}:{(start_time % 60000) // 1000:02},{start_time % 1000:03}"
+                end_srt = f"{end_time // 3600000:02}:{(end_time % 3600000) // 60000:02}:{(end_time % 60000) // 1000:02},{end_time % 1000:03}"
+
+                words_str = ' '.join(temp_words)
+                srt_content += f"{counter}\n{start_srt} --> {end_srt}\n{words_str}\n\n"
                 continue
 
-            # Start and end times
-            start_time = int(item["start"] * 1000)  # Convert to milliseconds
-            end_time = int(item["end"] * 1000)  # Convert to milliseconds
+            # if the next word is capitalized, write the group, but only if the current word is not capitalized
+            if i + 1 < len_of_aligned_object and aligned_object["words"][i + 1]["word"][0].isupper() and aligned_object["words"][i]["word"][0].islower():
+                start_time = int(temp_start * 1000)
+                end_time = int(temp_end * 1000)
 
-            # Format times as SRT requires
+                start_srt = f"{start_time // 3600000:02}:{(start_time % 3600000) // 60000:02}:{(start_time % 60000) // 1000:02},{start_time % 1000:03}"
+                end_srt = f"{end_time // 3600000:02}:{(end_time % 3600000) // 60000:02}:{(end_time % 60000) // 1000:02},{end_time % 1000:03}"
+
+                words_str = ' '.join(temp_words)
+                srt_content += f"{counter}\n{start_srt} --> {end_srt}\n{words_str}\n\n"
+                counter += 1
+
+                temp_words = []
+                temp_start = None
+                temp_end = None
+                continue
+
+            if len(temp_words) == group_size:
+                start_time = int(temp_start * 1000)
+                end_time = int(temp_end * 1000)
+
+                start_srt = f"{start_time // 3600000:02}:{(start_time % 3600000) // 60000:02}:{(start_time % 60000) // 1000:02},{start_time % 1000:03}"
+                end_srt = f"{end_time // 3600000:02}:{(end_time % 3600000) // 60000:02}:{(end_time % 60000) // 1000:02},{end_time % 1000:03}"
+
+                words_str = ' '.join(temp_words)
+                srt_content += f"{counter}\n{start_srt} --> {end_srt}\n{words_str}\n\n"
+                counter += 1
+
+                temp_words = []
+                temp_start = None
+                temp_end = None
+
+        # Handle any remaining words that didn't make a full group
+        if temp_words:
+            start_time = int(cast(int,temp_start) * 1000)
+            end_time = int(cast(int, temp_end) * 1000)
+
             start_srt = f"{start_time // 3600000:02}:{(start_time % 3600000) // 60000:02}:{(start_time % 60000) // 1000:02},{start_time % 1000:03}"
             end_srt = f"{end_time // 3600000:02}:{(end_time % 3600000) // 60000:02}:{(end_time % 60000) // 1000:02},{end_time % 1000:03}"
 
-            # Adding the text
-            srt_content += f"{counter}\n{start_srt} --> {end_srt}\n{item['word']}\n\n"
-            counter += 1
+            words_str = ' '.join(temp_words)
+            srt_content += f"{counter}\n{start_srt} --> {end_srt}\n{words_str}\n\n"
 
         with open(output_path, "w") as f:
             f.write(srt_content)
+
+    def clean_up_transcript(self, transcript: str) -> str:
+        """
+        Cleans up the transcript for better alignment accuracy.
+
+        Args:
+            transcript (str): The transcript to be cleaned up.
+
+        Returns:
+            str: The cleaned up transcript.
+        """
+
+        pattern = r"\b(\w+)-(\w+)\b"
+        result = re.sub(pattern, r"\1 \2", transcript)
+
+        result = result.replace(":", " ")
+
+        return result
+
+
