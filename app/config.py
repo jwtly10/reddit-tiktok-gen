f350ca9305a5dd6e4cc410ec31628fc73384cb30
Josh Watley <watleyjosh@gmail.com>
2024-04-21 17:23:20 +0000

    fix: checking for dependencies pre run

--- app/config.py
+++ app/config.py
@@ -1,7 +1,30 @@
 import os
 from dotenv import load_dotenv
-
+import matplotlib.font_manager as font_manager
 from app.utils.logger import log
+
+import subprocess
+
+def check_ffmpeg_installed():
+    try:
+        output = subprocess.run(["ffmpeg", "-version"], capture_output=True, text=True, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def check_process_running(process_name):
+    try:
+        output = subprocess.run(["pgrep", "-af", process_name], capture_output=True, text=True)
+        return process_name in output.stdout
+    except subprocess.CalledProcessError:
+        return False
+
+
+def check_fonts_installed(font_names):
+    fonts = set(f.fname for f in font_manager.fontManager.ttflist)
+    installed_fonts = {font: any(font in f for f in fonts) for font in font_names}
+    return installed_fonts
 
 
 def load_env():
@@ -16,6 +39,20 @@ def load_env():
     missing_vars = [var for var in required_vars if not os.getenv(var)]
     if missing_vars:
         raise EnvironmentError(f"Missing environment var(s) {', '.join(missing_vars)}")
+
+    # Check all dependencies
+    if not check_ffmpeg_installed():
+        raise EnvironmentError("ffmpeg is not installed")
+    if not check_process_running("gentle"):
+        raise EnvironmentError("gentle aligner is not running")
+
+    # Check required fonts
+    font_names = ["Poppins-SemiBold.tff", "Mont"]
+    installed_fonts = check_fonts_installed(font_names)
+    for font, installed in installed_fonts.items():
+        if not installed:
+            pass
+            # raise EnvironmentError(f"Font {font} is not installed")
 
     log.info(f"Running in {os.getenv("ENV")}")
 
@@ -46,8 +83,8 @@ ffmpeg_config = {
         "preset": "slow",
         "crf": 18,
         "capture_stdout": False,
-        "capture_stderr": True,
-        "global_args": [],
+        "capture_stderr": False,
+        "global_args": ["-loglevel", "info"],
     },
     "low_quality": {
         "preset": "ultrafast",
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Some basic cmds to init ssh machine
+# Some basic cmds to init ssh machine (ubuntu)
 
 # Clone the repo
 git clone https://github.com/jwtly10/reddit-tiktok-gen.git
@@ -10,4 +10,24 @@ source .venv/bin/activate
 
 # Install the requirements
 pip install -r requirements.txt
+
+# Install deps
+sudo apt install ffmpeg
+
+# Run deps
+docker run -p 8765:8765 lowerquality/gentle
+
+# Install fonts
+sudo cp assets/Poppins-SemiBold.ttf /usr/share/fonts/
+sudo cp assets/Mont-HeavyDEMO.otf /usr/share/fonts/
+sudo fc-cache -fv
+
+# Copy background media file(s) to the remote machine
+scp ./assets/minecraft_background_video_1.mp4 user@host:/root/reddit-tiktok-gen/assets/
+
+
+# Copy output files to local machine
+mkdir ./ssh-output
+scp -r user@host:/root/reddit-tiktok-gen/automation ./ssh-output
+
 
